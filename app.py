215bd32f298489aa95d7856222b62ccc9dbf9c9a
Bernardo (UC3M) <bernardo.dauria@uc3m.es>
2021-03-10 00:06:29 +0100

    graph filtered

--- app.py
+++ app.py
@@ -27,11 +27,10 @@ markdown_text = '''
 
 table_tab = dash_table.DataTable(
                 id='my-table',
-                columns=[{"name": i, "id": i} for i in df.columns],
-                data= df.to_dict("records")
+                columns=[{"name": i, "id": i} for i in df.columns]
             )
 
-graph_tab = dcc.Graph(id="graph", figure= px.scatter(df, x="bodywt", y="sleep_total", color="vore"))
+graph_tab = dcc.Graph(id="my-graph")
 
 app.layout= html.Div([
     html.Div([html.H1(app.title, className="app-header--title")],
@@ -61,11 +60,23 @@ def render_content(tab):
 
 @app.callback(
      Output('my-table', 'data'),
-     Input('my-dropdown', 'value'))
-def update_data(values):
+     Input('my-dropdown', 'value'),
+     State('tabs', 'value'))
+def update_table(values, tab):
+    if tab != 'tab-t':
+        return None
     filter = df['vore'].isin(values)
     return df[filter].to_dict("records")
 
+@app.callback(
+     Output('my-graph', 'figure'),
+     Input('my-dropdown', 'value'),
+     State('tabs', 'value'))
+def update_graph(values, tab):
+    if tab != 'tab-g':
+        return None
+    filter = df['vore'].isin(values)
+    return px.scatter(df[filter], x="bodywt", y="sleep_total", color="vore")
 
 if __name__ == '__main__':
     app.server.run(debug=True)
