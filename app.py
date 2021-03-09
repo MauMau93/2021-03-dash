013fceaeb19cd74d9f7b419e4907991288739d84
Bernardo (UC3M) <bernardo.dauria@uc3m.es>
2021-03-09 22:05:05 +0100

    added DataTable

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 import dash
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_table
+
+import pandas as pd
+
+app = dash.Dash(__name__, title="2021 Dash Python App")
 
 markdown_text = '''
 ### Some references
@@ -9,11 +14,17 @@ markdown_text = '''
 - [Dash Bootstrap Components](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/)  
 '''
 
-app = dash.Dash(__name__, title="2021 Dash Python App")
+df_url = 'https://forge.scilab.org/index.php/p/rdataset/source/file/master/csv/ggplot2/msleep.csv'
+df = pd.read_csv(df_url)
 
 app.layout = html.Div([
     html.H1(app.title),
-    dcc.Markdown(markdown_text)
+    dcc.Markdown(markdown_text),
+    dash_table.DataTable(
+        id='my-table',
+        columns=[{"name": i, "id": i} for i in df.columns],
+        data= df.to_dict("records")
+        )
 ])
 
 if __name__ == '__main__':
