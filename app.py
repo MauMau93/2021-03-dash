a9ce54534fc699cc4f573e2a49c541d307990283
Bernardo (UC3M) <bernardo.dauria@uc3m.es>
2021-03-09 23:33:40 +0100

    adding tabs

--- app.py
+++ app.py
@@ -3,11 +3,18 @@ from dash.dependencies import Input, Output, State
 
 import dash_html_components as html
 import dash_core_components as dcc
+import dash_bootstrap_components as dbc
 import dash_table
+
+app = dash.Dash(__name__, title="2021 Dash Python App")
 
 import pandas as pd
 
-app = dash.Dash(__name__, title="2021 Dash Python App")
+df_url = 'https://forge.scilab.org/index.php/p/rdataset/source/file/master/csv/ggplot2/msleep.csv'
+df = pd.read_csv(df_url)
+
+df_vore = df['vore'].dropna().sort_values().unique()
+opt_vore = [{'label': x + 'vore', 'value': x} for x in df_vore]
 
 markdown_text = '''
 ### Some references
@@ -17,11 +24,17 @@ markdown_text = '''
 - [Dash DataTable](https://dash.plotly.com/datatable)  
 '''
 
-df_url = 'https://forge.scilab.org/index.php/p/rdataset/source/file/master/csv/ggplot2/msleep.csv'
-df = pd.read_csv(df_url)
-
-df_vore = df['vore'].dropna().sort_values().unique()
-opt_vore = [{'label': x + 'vore', 'value': x} for x in df_vore]
+table_tab = dash_table.DataTable(
+                id='my-table',
+                columns=[{"name": i, "id": i} for i in df.columns],
+                data= df.to_dict("records")
+            )
+graph_tab = dcc.Graph(id="graph",
+                figure={"data":[
+                        {"x":[1,2,3], "y":[4,2,1], "type": "bar", "name": "DF"},
+                        {"x":[1,2,3], "y":[2,4,5], "type": "bar", "name": u'Montréal'}
+                        ]}
+            )
 
 app.layout= html.Div([
     html.Div([html.H1(app.title, className="app-header--title")],
@@ -32,14 +45,22 @@ app.layout= html.Div([
         html.Label(["Select types of feeding strategies:", 
             dcc.Dropdown('my-dropdown', options= opt_vore, value= [opt_vore[0]['value']], multi=True)
         ]),
-        dash_table.DataTable(
-            id='my-table',
-            columns=[{"name": i, "id": i} for i in df.columns],
-            data= df.to_dict("records")
-        )
+        dcc.Tabs(id="tabs", value='tab-t', children=[
+            dcc.Tab(label='Table', value='tab-t'),
+            dcc.Tab(label='Graph', value='tab-g'),
+        ]),
+        html.Div(id='tabs-content')
     ],
     className= "app-body")
 ])
+
+@app.callback(Output('tabs-content', 'children'),
+              Input('tabs', 'value'))
+def render_content(tab):
+    if tab == 'tab-t':
+        return table_tab
+    elif tab == 'tab-g':
+        return graph_tab
 
 @app.callback(
      Output('my-table', 'data'),
