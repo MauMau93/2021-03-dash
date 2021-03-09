dd342ec4f2ca0970b3c6b491ef6c94f1755eba64
Bernardo (UC3M) <bernardo.dauria@uc3m.es>
2021-03-09 21:38:20 +0100

    adding markdown content

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 import dash
 import dash_html_components as html
+import dash_core_components as dcc
+
+markdown_text = '''
+### Some references
+- [Dash HTML Components](https://dash.plotly.com/dash-html-components)
+- [Dash Core Components](https://dash.plotly.com/dash-core-components)  
+- [Dash Bootstrap Components](https://dash-bootstrap-components.opensource.faculty.ai/docs/components/)  
+'''
 
 app = dash.Dash(__name__, title="2021 Dash Python App")
 
-app.layout = html.H1(app.title) 
+app.layout = html.Div([
+    html.H1(app.title),
+    dcc.Markdown(markdown_text)
+])
 
 if __name__ == '__main__':
-    app.server.run()
+    app.server.run(debug=True)
