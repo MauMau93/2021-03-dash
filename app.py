34524f3cff976c8f5479b8074d604324bb0b3db4
Bernardo (UC3M) <bernardo.dauria@uc3m.es>
2021-03-10 11:32:55 +0100

    adding hidden dataRange

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ app = dash.Dash(__name__, title="2021 Dash Python App")
 
 import numpy as np
 import pandas as pd
+import json
 
 df_url = 'https://forge.scilab.org/index.php/p/rdataset/source/file/master/csv/ggplot2/msleep.csv'
 df = pd.read_csv(df_url).dropna(subset = ['vore'])
@@ -62,6 +63,7 @@ app.layout= html.Div([
                  )
         ]),
         html.Div(id='data', style={'display': 'none'}),
+        html.Div(id='dataRange', style={'display': 'none'}),
         dcc.Tabs(id="tabs", value='tab-t', children=[
             dcc.Tab(label='Table', value='tab-t'),
             dcc.Tab(label='Graph', value='tab-g'),
@@ -111,6 +113,16 @@ def filter(range, values):
      # more generally, this line would be
      # json.dumps(cleaned_df)
      return df[filter].to_json(date_format='iso', orient='split')
+
+
+@app.callback(Output('dataRange', 'children'), 
+    Input('my-dropdown', 'value'))
+def dataRange(values):
+    filter = df['vore'].isin(values) 
+    dff = df[filter]
+    min_bodywt = min(dff['bodywt'].dropna())
+    max_bodywt = max(dff['bodywt'].dropna())
+    return json.dumps({'min_bodywt': min_bodywt, 'max_bodywt': max_bodywt})
 
 if __name__ == '__main__':
     app.server.run(debug=True)
