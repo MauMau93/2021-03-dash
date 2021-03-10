1c67dd580f9f10c09cd709eb76a17b573a6fc17f
Bernardo (UC3M) <bernardo.dauria@uc3m.es>
2021-03-10 11:16:08 +0100

    filtering on RangeSlider with logarithmic scale

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import dash_table
 
 app = dash.Dash(__name__, title="2021 Dash Python App")
 
+import numpy as np
 import pandas as pd
 
 df_url = 'https://forge.scilab.org/index.php/p/rdataset/source/file/master/csv/ggplot2/msleep.csv'
@@ -31,6 +32,10 @@ markdown_text = '''
 - [Dash DataTable](https://dash.plotly.com/datatable)  
 '''
 
+def slider_map(min, max, steps=10):
+    scale = np.logspace(np.log10(min), np.log10(max), steps, endpoint=False)
+    return {i/10: '{}'.format(round(scale[i],2)) for i in range(steps)}
+
 table_tab = dash_table.DataTable(
                 id='my-table',
                 columns=[{"name": i, "id": i} for i in df.columns]
@@ -48,12 +53,12 @@ app.layout= html.Div([
             dcc.Dropdown('my-dropdown', options= opt_vore, value= [opt_vore[0]['value']], multi=True)
         ]),
         html.Label(["Range of values for body weight:", 
-                 dcc.RangeSlider(
-                     max= max_bodywt,
-                     min= min_bodywt,
-                     step= (max_bodywt - min_bodywt)/10,
-                     marks= {'{}'.format(min_bodywt + (max_bodywt - min_bodywt)/10 * i): '{}'.format(round(min_bodywt + (max_bodywt - min_bodywt)/10 * i,2)) for i in range(10)},
-                     value= [min_bodywt,max_bodywt],
+                 dcc.RangeSlider(id="range",
+                     max= 1,
+                     min= 0,
+                     step= 1/100,
+                     marks= slider_map(min_bodywt, max_bodywt),
+                     value= [0,1],
                  )
         ]),
         dcc.Tabs(id="tabs", value='tab-t', children=[
@@ -73,25 +78,25 @@ def render_content(tab):
     elif tab == 'tab-g':
         return graph_tab
 
+
 @app.callback(
      Output('my-table', 'data'),
-     Input('my-dropdown', 'value'),
-     State('tabs', 'value'))
-def update_table(values, tab):
+     Input('range', 'value'), 
+     State('my-dropdown', 'value'), State('tabs', 'value'))
+def update_table(range, values, tab):
     if tab != 'tab-t':
         return None
-    filter = df['vore'].isin(values)
+    filter = df['vore'].isin(values) & df['bodywt'].between(min_bodywt * (max_bodywt/min_bodywt) ** range[0], min_bodywt * (max_bodywt/min_bodywt) ** range[1])
     return df[filter].to_dict("records")
 
 @app.callback(
      Output('my-graph', 'figure'),
-     Input('my-dropdown', 'value'),
-     State('tabs', 'value'))
-def update_graph(values, tab):
+     Input('range', 'value'), 
+     State('my-dropdown', 'value'), State('tabs', 'value'))
+def update_graph(range, values, tab):
     if tab != 'tab-g':
         return None
-    filter = df['vore'].isin(values)
-    print(col_vore)
+    filter = df['vore'].isin(values) & df['bodywt'].between(min_bodywt * (max_bodywt/min_bodywt) ** range[0], min_bodywt * (max_bodywt/min_bodywt) ** range[1])
     return px.scatter(df[filter], x="bodywt", y="sleep_total", color="vore",
     #color_discrete_sequence=px.colors.qualitative.G10
     color_discrete_map=col_vore)
